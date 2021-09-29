9639a86237ade0a804ad93d8155acfc8444460cb
Noëlla Zwaneveld <89777909+NoellaZwaneveld@users.noreply.github.com>
2021-09-29 09:33:40 +0200

    1

---
+++ person.py
@@ -0,0 +1,30 @@
+a = '''
+----------------------------------------------------
+'''
+
+input('verzoek')
+
+print ('Naaminvoer')
+name = input('Geef hier jou voor + achternaam ')
+
+print ('')
+
+print ('Adresinvoer')
+adres = input ('Geef hier jou adres ')
+
+print ('')
+
+print ('Postcodeinvoer')
+postcode = input ('Geef hier jou postcode ')
+
+print ('')
+
+print ('Woonplaatsinvoer')
+woonplaats = input ('Geef hier jou woonplaats ')
+
+print(a)
+print ('Naam: ' + name)
+print ('Adres: ' + adres)
+print ('postcode: ' + postcode)
+print ('Woonplaats: ' + woonplaats)
+print(a)
